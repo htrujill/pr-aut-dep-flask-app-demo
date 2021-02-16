23fc7148e0ab5968e19eab2b2a1c16b5768e2ff5
Hector Trujillo Ruiz <htrujillo@palace-resorts.local>
2021-02-16 12:42:45 -0500

    functions response changed

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World! this is a test"
+    resp = {'code': '200', 'message': 'blue/green deployment successfully executed!'}
+    return jsonify(resp)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
